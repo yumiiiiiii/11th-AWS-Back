40fbfe9913b3ee4fd1aa8d56643e09f5b4e8cee5
yumiiiiiii <102947194+yumiiiiiii@users.noreply.github.com>
2023-09-06 21:40:07 +0900

    cors 보안 추가

--- aws_project/settings/base.py
+++ aws_project/settings/base.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+     "corsheaders.middleware.CorsMiddleware", #cors 보안
     "django.middleware.common.CommonMiddleware",
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
